c9481b71df1a4ac67041fba679c58c4a878ffbd7
Sebastian Kolsut <skolsut@c3r10s3.42warsaw.pl>
2026-01-29 16:53:40 +0100

    Player class functional

--- game/Player.py
+++ game/Player.py
@@ -1,5 +1,5 @@
 from MAZE import MazeManager
-from enums import Key, Numpad, Arrow, Direction
+from enums import Arrow, Direction
 from typing import List
 
 
@@ -8,9 +8,18 @@ class Player:
         self.current_position = maze.entry
         self.taret_position = maze.exit
         self.maze_map = maze.map
-        self.path: List[int] = []
+        self.path: List[int] = [maze.entry]
 
-    def move(self, key_pressed: Key) -> List[int]:
+    def move(self, key_pressed: Arrow) -> List[int]:
+        direction = self._get_direction(key_pressed)
+
+        if direction is not None and self._is_valid_move(direction):
+            self.path.append(self._get_next_cell(direction))
+
+        return self.path
+
+    @staticmethod
+    def _get_direction(key_pressed: Arrow):
         direction: Direction = None
 
         if key_pressed == Arrow.UP:
@@ -22,11 +31,19 @@ class Player:
         elif key_pressed == Arrow.LEFT:
             direction = Direction.WEST
 
-        if direction is not None and self.is_valid_move(direction):
-            
+        return direction
 
-    def is_valid_move(self, direction: Direction) -> bool:
+    def _is_valid_move(self, direction: Direction) -> bool:
         val = self.maze[self.current_position]
 
         return not (val & (1 << direction.value))
 
+    def _get_next_cell(self, direction: Direction) -> int:
+        if direction == Direction.NORTH:
+            return self.current_position - self.width
+        if direction == Direction.SOUTH:
+            return self.current_position + self.width
+        if direction == Direction.EAST:
+            return self.current_position + 1
+        if direction == Direction.WEST:
+            return self.current_position - 1
